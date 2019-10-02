c7c7ea2982947e2c2888116ff35c150fa9365edb
eric-chen <eric-pw@qq.com>
2019-10-02 17:57:22 +0800

    商品详情页

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,61 @@
 // pages/goods_detail/index.js
+import {request} from "../../request/index.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    goodsObj:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad(opt){
+    // console.log(opt)
+    this.getGoodsDetail(opt.goods_id)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getGoodsDetail(goods_id){
+    request({
+      url:"/goods/detail",
+      data:{
+        goods_id
+      }
+    }).then(res=>{
+      // console.log(res)
+      this.setData({
+        goodsObj:res.data.message
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 点击轮播图
+  hadleImagePreview(e){
+    const {goodsObj} =this.data
+    const urls=goodsObj.pics.map(v=>v.pics_mid_url)
+    const current=e.currentTarget.dataset.current
+    wx.previewImage({
+      current: current,
+      urls: urls
+    });
+      
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点击加入购物车
+  handleCartAdd(){
+    const {goodsObj}=this.data
+    let cartList=wx.getStorageSync("carts") || [];
+    const index=cartList.findIndex(v=>v.goods_id===goodsObj.goods_id)
+    if(index===-1){
+      cartList.push({
+        goods_id:goodsObj.goods_id,
+        goods_name:goodsObj.goods_name,
+        goods_price:goodsObj.goods_price,
+        goods_small_logo:goodsObj.goods_small_logo,
+        num:1
+      })
+    }else{
+      cartList[index].num++;
+    }
+    wx.setStorageSync("carts", cartList);
+    // 弹出窗口提示用户加入购物车成功
+    wx.showToast({
+      title: '加入购物车成功',
+      mask:true,
+      icon: 'success'
+    });
+      
+      
   }
 })
